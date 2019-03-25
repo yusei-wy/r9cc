fa2c7bf6776c818b9e18101bbaca47796c05e7e1
yuse-wy <yusei.kasa@gmail.com>
2019-03-25 10:46:07 +0900

    足し算と引き算を追加

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,57 @@
 use std::env;
 
+fn strtonum(p: &mut String) -> i32 {
+    let mut result = String::from("");
+
+    for (i, c) in p.bytes().enumerate() {
+        if c.is_ascii_digit() {
+            result.push(c as char);
+        } else {
+            break;
+        }
+    }
+
+    *p = p[result.len()..].to_string();
+
+    result.parse::<i32>().unwrap()
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut args = env::args();
     if args.len() != 2 {
         panic!("引数の個数が正しくありません\n");
     }
+    args.next();
+
+    let mut p: String = match args.next() {
+        Some(arg) => arg,
+        None => panic!("引数を取得できませんでした"),
+    };
 
     println!(".intel_syntax noprefix");
     println!(".global _main");
     println!("_main:");
-    println!("  mov rax, {}", &args[1].parse::<i32>().unwrap());
+    println!("  mov rax, {}", strtonum(&mut p));
+
+    loop {
+        if let Some(b) = p.bytes().next() {
+            if b == b'+' {
+                p = p[1..].to_string();
+                println!("  add rax, {}", strtonum(&mut p));
+                continue;
+            }
+
+            if b == b'-' {
+                p = p[1..].to_string();
+                println!("  sub rax, {}", strtonum(&mut p));
+                continue;
+            }
+
+            panic!("予期しない文字です: {}", b as char);
+        } else {
+            break;
+        }
+    }
+
     println!("  ret");
 }
--- tmp.s
+++ tmp.s
@@ -1,3 +1,7 @@
 .intel_syntax noprefix
 .global _main
 _main:
+  mov rax, 5
+  add rax, 20
+  sub rax, 4
+  ret
